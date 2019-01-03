80b7c43f102e85418ffe584ce146ebd869c61ec3
Luis Castro Martín <luiscastro193@gmail.com>
2019-01-03 19:18:02 +0100

    Add files via upload

---
+++ graph.js
@@ -0,0 +1,48 @@
+"use strict";
+const workerURL = 'lite.render.js';
+let viz = new Viz({ workerURL });
+let inputElement = document.getElementById('inputElement');
+let graphSection = document.getElementById('graphSection');
+
+function gvString(links) {
+	return `digraph {
+
+graph [rankdir = "LR"];
+graph [nodesep = 0.5];
+node [fontname = "Helvetica"];
+node [fontsize = 11];
+node [shape = box];
+node [width = 0];
+node [height = 0];
+node [margin = 0.1];
+
+${links}
+
+}`;
+}
+
+function notationToLinks(graph) {
+	graph = graph.replace(/^[^"\n\r]+$/gm, '"$&"');
+	graph = graph.replace(/ gto /g, '" -> "');
+	graph = graph.replace(/"[^"]+ gand [^"]+"/g, '{$&}');
+	graph = graph.replace(/ gand /g, '" "');
+	graph = graph.replace(/"[^"]+ gsame [^"]+"/g, '{rank=same $&}');
+	graph = graph.replace(/ gsame /g, '" "');
+	graph = graph.replace(/ gconstraint"/g, '" [constraint = false]');
+	
+	return graph;
+}
+
+function elementPromise(input) {
+	return viz.renderSVGElement(gvString(notationToLinks(input)));
+}
+
+function updateGraph() {
+	elementPromise(inputElement.value).then(element => {
+		graphSection.innerHTML = '';
+		graphSection.appendChild(element);
+	}).catch(error => {
+		viz = new Viz({ workerURL });
+		console.error(error);
+	});
+}
