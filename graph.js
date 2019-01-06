f1396b9bd5093b6920f1e8550bc0c4cae1d895d0
luis <luiscastro193@gmail.com>
2019-01-06 09:09:46 +0100

    Greedy quantifiers caveats

--- graph.js
+++ graph.js
@@ -23,9 +23,9 @@ function notationToLinks(graph) {
 	graph = graph.replace(/ $/gm, '');
 	graph = graph.replace(/^[^"]+?$/gm, '"$&"');
 	graph = graph.replace(/ gto /g, '" -> "');
-	graph = graph.replace(/"[^"]+ gand .*?"/g, '{$&}');
+	graph = graph.replace(/"[^"]* gand [^"]*"/g, '{$&}');
 	graph = graph.replace(/ gand /g, '" "');
-	graph = graph.replace(/"[^"]+ gsame .*?"/g, '{rank=same $&}');
+	graph = graph.replace(/"[^"]* gsame [^"]*"/g, '{rank=same $&}');
 	graph = graph.replace(/ gsame /g, '" "');
 	graph = graph.replace(/ gconstraint"/g, '" [constraint = false]');
 	
