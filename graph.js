2b99a36a16fa549a491e19ddc03c3aad19759adb
luis <luiscastro193@gmail.com>
2023-01-04 19:19:32 +0100

    Smoother transition

--- graph.js
+++ graph.js
@@ -19,7 +19,7 @@ function gvString(links) {
 }
 
 function notationToLinks(graph) {
-	graph = graph.replace(/\\? ?$/gm, '');
+	graph = graph.replace(/[ \\]+$/gm, '');
 	graph = graph.replace(/^[^"]+?$/gm, '"$&"');
 	graph = graph.replace(/ gto /g, '" -> "');
 	graph = graph.replace(/"[^"]* gand [^"]*"/g, '{$&}');
